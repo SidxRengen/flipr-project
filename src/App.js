770c032a3967ee2e2f02e19f14123d434d017934
SidxRengen <gautamsiddharth226@gmail.com>
2023-07-30 16:27:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./component/Navbar";
 
 function App() {
-  
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/nav" element={<Navbar />} />
+      </Routes>
     </div>
   );
 }
